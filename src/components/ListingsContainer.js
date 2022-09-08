a0bcca7417c65db54903b9b7751a17a7094531b4
Bailey Cage <bailey.cage@gmail.com>
2022-09-08 10:37:35 -0400

    fetch listings from server

--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 // import ListingCard from "./ListingCard";
 
 function ListingsContainer() {
+
+  const [listingsAry, setListingsAry] = useState([]);
+
+  useEffect(()=>{
+    fetch(`http://localhost:6001/listings`)
+    .then(r => r.json())
+    .then(data => setListingsAry(data));
+  }, []);
+
   return (
     <main>
       <ul className="cards">
